21f9a04870269abd372fcbdc99402f3447665289
njacob1001 <njacob1001@gmail.com>
2018-10-26 22:30:35 -0500

    version 1-2

---
+++ public/chunk.post.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3hrQ":function(n,o,e){"use strict";e.r(o);var r=e("mXGw"),t=e.n(r),a=function(n){var o=n.match;return console.log("olovotgo"),t.a.createElement("h1",null,"Hey hey ",o.params.id)};e.d(o,"Component",function(){return c});var c=a}}]);
